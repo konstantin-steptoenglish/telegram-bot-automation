1c7b3fa722177d91a468d7982896241e95de31a1
Konstantin <konstantin@steptoenglish.com>
2026-04-17 17:51:42 +0200

    Fix event loop order - create loop before scheduler.start()

--- main.py
+++ main.py
@@ -35,13 +35,13 @@ class TelegramBot:
             logger.warning('Post rejected - red flags detected')
 
     def start(self):
-        self.scheduler.add_job(self.publish_scholarship_post, 'cron', hour=8, minute=0, timezone=TIMEZONE, id='scholarship_post')
-        self.scheduler.add_job(self.publish_lifehack_post, 'cron', hour=14, minute=30, timezone=TIMEZONE, id='lifehack_post')
-        self.scheduler.start()
-        logger.info('Bot started and scheduler running')
         try:
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
+            self.scheduler.add_job(self.publish_scholarship_post, 'cron', hour=8, minute=0, timezone=TIMEZONE, id='scholarship_post')
+            self.scheduler.add_job(self.publish_lifehack_post, 'cron', hour=14, minute=30, timezone=TIMEZONE, id='lifehack_post')
+            self.scheduler.start()
+            logger.info('Bot started and scheduler running')
             loop.run_forever()
         except KeyboardInterrupt:
             self.scheduler.shutdown()
